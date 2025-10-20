18ab602d3d5a8b8ec2773c05bca430a5317bc2f9
rey <krizaxxc@gmail.com>
2025-10-20 23:43:48 +0200

    Initial commit for ImageGenerator project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageGenerator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+
+namespace ImageGenerator
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        private int currentIndex = 0;
+
+        private List<string> imageList = new()
+        {
+            "first",
+            "second",
+            "third",
+            "forth"
+        };
+
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ShowImage();
+        }
+
+        private void ShowImage()
+        {
+#if WINDOWS
+            ShowGallery.Source = $"{imageList[currentIndex]}.jpg";
+#else
+            ShowGallery.Source = imageList[currentIndex];
+#endif
+            
+        }
+
+        private void NextClicked(object sender, EventArgs e)
+        {
+            currentIndex++;
+            if (currentIndex >= imageList.Count)
+                currentIndex = 0; 
+
+            ShowImage();
+        }
+
+        private void PreviousClicked(object sender, EventArgs e)
+        {
+            currentIndex--;
+            if (currentIndex < 0)
+                currentIndex = imageList.Count - 1;
+            
+            ShowImage();
+        }
+    }
+}
